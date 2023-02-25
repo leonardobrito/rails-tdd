9a03ff4bfd8484218505255fe90cd9f17320e97e
leonardobrito <f.leobrito@gmail.com>
2023-02-25 15:17:17 -0300

    :speech_balloon: One line sintaxe

--- rspec/spec/matchers/ranges/ranges_spec.rb
+++ rspec/spec/matchers/ranges/ranges_spec.rb
@@ -2,8 +2,15 @@
 
 describe (1..5), 'Ranges' do
   it '#cover' do
+    is_expected.to cover(2) # same as below
     expect(subject).to cover(2)
     expect(subject).to cover(5, 2) # order doesn't matter
     expect(subject).not_to cover(0, 6) # order doesn't matter
   end
+
+  # one-line sintaxe
+  it { expect(subject).to cover(2) }
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(5, 2) }
+  it { is_expected.not_to cover(0, 6) }
 end
