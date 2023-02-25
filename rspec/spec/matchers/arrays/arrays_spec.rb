7f4db5470f8990f6e07f35367c6808bb830d92be
leonardobrito <f.leobrito@gmail.com>
2023-02-25 17:25:34 -0300

    :heavy_minus_sign: Negative matchers

--- rspec/spec/matchers/arrays/arrays_spec.rb
+++ rspec/spec/matchers/arrays/arrays_spec.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1, 2, 3]), 'Array' do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(2, 1) # order doesn't matter
+    expect(subject).to exclude(4)
   end
+
+  it { expect(subject).to exclude(4) }
 
   it '#contain_exactly' do
     expect(subject).to contain_exactly(2, 1, 3) # order doesn't matter
--- rspec/spec/matchers/be_within/be_within_spec.rb
+++ rspec/spec/matchers/be_within/be_within_spec.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+RSpec::Matchers.define_negated_matcher :be_not_within, :be_within
+
 describe 'be_within' do
   it { expect(11.4).not_to be_within(0.5).of(12) }
+  it { expect(11.4).to be_not_within(0.5).of(12) }
   it { expect(11.5).to be_within(0.5).of(12) }
   it { expect(12.5).to be_within(0.5).of(12) }
   it { expect(12.6).not_to be_within(0.5).of(12) }
