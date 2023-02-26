e02e49bd88beb291f38e98b8a2bf171e6ef238e8
leonardobrito <f.leobrito@gmail.com>
2023-02-26 00:10:12 -0300

    :clown_face: More about stubs

--- rspec/lib/student.rb
+++ rspec/lib/student.rb
@@ -5,4 +5,7 @@ class Student
   attr_accessor :name, :email
 
   def finished?(course); end
+
+  def foo(arg); end
+  def bar; end
 end
--- rspec/spec/stubs/stubs_spec.rb
+++ rspec/spec/stubs/stubs_spec.rb
@@ -8,10 +8,47 @@ describe 'Stubs' do
     student = Student.new
     course = Course.new
 
+    # allow(student).to receive(:finished?)
+    #   .and_return(true)
+
     allow(student).to receive(:finished?)
       .with(an_instance_of(Course))
       .and_return(true)
 
     expect(student.finished?(course)).to be_truthy
+  end
+
+  it '#foo, dinamic args' do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      case arg
+      when :hello
+        'Hello'
+      when :hi
+        'Hi'
+      end
+    end
+
+    expect(student.foo(:hello)).to be('Hello')
+    expect(student.foo(:hi)).to be('Hi')
+  end
+
+  it 'Any class instance' do
+    student = Student.new
+    other_student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(student.bar).to be_truthy
+    expect(other_student.bar).to be_truthy
+  end
+
+  it 'Error' do
+    student = Student.new
+
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect { student.bar }.to raise_error(RuntimeError)
   end
 end
