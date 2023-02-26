64d7afd7f191ac211e8317a92c84ade88f65128a
leonardobrito <f.leobrito@gmail.com>
2023-02-26 00:21:00 -0300

    :clown_face: More mocks

--- rspec/spec/mocks/mocks_spec.rb
+++ rspec/spec/mocks/mocks_spec.rb
@@ -13,4 +13,34 @@ describe 'Mock' do
     # exercise
     student.bar
   end
+
+  it '#foo with args' do
+    student = Student.new
+
+    expect(student).to receive(:foo).with(123)
+
+    student.foo(123)
+  end
+
+  it '#foo repetitions call' do
+    student = Student.new
+
+    # expect(student).to receive(:foo).with(123).once
+    expect(student).to receive(:foo).with(123).twice
+    # expect(student).to receive(:foo).with(123).exactly(2).times
+    # expect(student).to receive(:foo).with(123).at_least(:once)
+    # expect(student).to receive(:foo).with(123).at_least(:twice)
+    # expect(student).to receive(:foo).with(123).at_least(2).times
+
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it '#foo with args and return' do
+    student = Student.new
+
+    expect(student).to receive(:foo).with(123).and_return(true)
+
+    expect(student.foo(123)).to be_truthy
+  end
 end
